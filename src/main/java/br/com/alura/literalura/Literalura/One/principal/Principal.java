80251b17493eb045de805ec26ec5df016f3bcaa4
Davi Costa <davim.costa@outlook.com>
2024-05-21 19:23:41 -0300

    desafio finalizado.

--- src/main/java/br/com/alura/literalura/Literalura/One/principal/Principal.java
+++ src/main/java/br/com/alura/literalura/Literalura/One/principal/Principal.java
@@ -1,9 +1,6 @@
 package br.com.alura.literalura.Literalura.One.principal;
 
-import br.com.alura.literalura.Literalura.One.model.Autor;
-import br.com.alura.literalura.Literalura.One.model.DadosAutor;
-import br.com.alura.literalura.Literalura.One.model.DadosLivro;
-import br.com.alura.literalura.Literalura.One.model.Livro;
+import br.com.alura.literalura.Literalura.One.model.*;
 import br.com.alura.literalura.Literalura.One.repository.LivroRepository;
 import br.com.alura.literalura.Literalura.One.service.ConsumoAPI;
 import br.com.alura.literalura.Literalura.One.service.ConverteDados;
@@ -12,9 +9,9 @@ import com.fasterxml.jackson.databind.ObjectMapper;
 
 import java.io.IOException;
 import java.util.Collections;
+import java.util.InputMismatchException;
 import java.util.List;
 import java.util.Scanner;
-import java.util.stream.Collectors;
 
 public class Principal {
 
@@ -34,6 +31,8 @@ public class Principal {
                     1 - Buscar livros pelo título
                     2 - Buscar livros registrados
                     3 - Buscar autores registrados
+                    4 - Buscar autores vivos em determinado ano
+                    5 - Buscar livros por idioma
                     0 - Sair                                 
                     """;
 
@@ -51,6 +50,12 @@ public class Principal {
                 case 3:
                     buscarAutoresRegistrados();
                     break;
+                case 4:
+                    buscarAutoresVivosEmDeterminadoAno();
+                    break;
+                case 5:
+                    buscarLivrosPorIdioma();
+                    break;
                 case 0:
                     System.out.println("Saindo...");
                     break;
@@ -60,29 +65,69 @@ public class Principal {
         }
     }
 
+    private void buscarLivrosPorIdioma() {
+        System.out.println("Escolha um idioma: ");
+        Linguagem linguagem = Linguagem.fromString(leitura.nextLine());
+
+        var livrosEncontrados = repositorio.findByLinguagens(linguagem);
+
+        if(livrosEncontrados.isPresent()) {
+            var livros = livrosEncontrados.get();
+            livros.forEach(System.out::println);
+        } else {
+            System.out.println("Livro não encontrada!");
+        }
+    }
+
+    private void buscarAutoresVivosEmDeterminadoAno() {
+        System.out.println("Digite um ano para busca: ");
+        try {
+            var anoDesejado = leitura.nextInt();
+            List<Autor> autoresVivos = repositorio.autoresVivosEmDeterminadoAno(anoDesejado);
+            if(autoresVivos.isEmpty()) {
+                System.out.println("Nenhum autor encontrado.");
+            }
+            autoresVivos.forEach(System.out::println);
+        } catch (InputMismatchException e) {
+            System.out.println("Entrada inválida! ");
+        }
+
+    }
+
     private void buscarAutoresRegistrados() {
         List<Autor> autoresRegistrados = repositorio.autoresRegistrados();
+        if (autoresRegistrados.isEmpty()) {
+            System.out.println("Não há nenhum autor cadastrado!");
+        }
         autoresRegistrados.forEach(System.out::println);
     }
 
     private void buscarLivrosRegistrados() {
         List<Livro> livrosRegistrados = repositorio.findAll();
+        if (livrosRegistrados.isEmpty()) {
+            System.out.println("Nenhum livro cadastrado");
+        }
         livrosRegistrados.forEach(System.out::println);
     }
 
     private void buscarLivroNaApi() {
         System.out.println("Digite o nome de um livro para busca: ");
         var nomeLivro = leitura.nextLine();
-        var json = consumo.obterDados("https://gutendex.com/books/?search=" + nomeLivro.replace(" ", "%20"));
-        var jsonConvertido = extrairResultados(json, "results");
-        DadosLivro dados = conversor.obterDados(jsonConvertido, DadosLivro.class);
-        List<DadosAutor> dadosAutor = Collections.singletonList(conversor.obterDados(extrairResultados(jsonConvertido, "authors"), DadosAutor.class));
-        Livro livro = new Livro(dados);
-        List<Autor> autores = dadosAutor.stream().map(a -> new Autor(a, livro)).toList();
-        livro.setAutores(autores);
-        repositorio.save(livro);
+        try {
+            var json = consumo.obterDados("https://gutendex.com/books/?search=" + nomeLivro.replace(" ", "%20"));
+            var jsonConvertido = extrairResultados(json, "results");
+            DadosLivro dados = conversor.obterDados(jsonConvertido, DadosLivro.class);
+            List<DadosAutor> dadosAutor = Collections.singletonList(conversor.obterDados(extrairResultados(jsonConvertido, "authors"), DadosAutor.class));
+            Livro livro = new Livro(dados);
+            List<Autor> autores = dadosAutor.stream().map(a -> new Autor(a, livro)).toList();
+            livro.setAutores(autores);
+            repositorio.save(livro);
 
-        System.out.println(livro);
+            System.out.println(livro);
+        } catch (NullPointerException e) {
+            System.out.println("Livro não encontrado ou não existente. \n ");
+        }
+
     }
 
     private String extrairResultados(String apiResponse, String objetivo) {
--- src/main/java/br/com/alura/literalura/Literalura/One/repository/LivroRepository.java
+++ src/main/java/br/com/alura/literalura/Literalura/One/repository/LivroRepository.java
@@ -1,14 +1,22 @@
 package br.com.alura.literalura.Literalura.One.repository;
 
 import br.com.alura.literalura.Literalura.One.model.Autor;
+import br.com.alura.literalura.Literalura.One.model.Linguagem;
 import br.com.alura.literalura.Literalura.One.model.Livro;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 
 import java.util.List;
+import java.util.Optional;
 
 public interface LivroRepository extends JpaRepository<Livro, Long> {
 
     @Query("SELECT a FROM Livro l JOIN l.autores a")
     List<Autor> autoresRegistrados();
+
+    @Query("SELECT a FROM Livro l JOIN l.autores a WHERE a.anoFalecimento > :ano AND a.anoNascimento < :ano")
+    List<Autor> autoresVivosEmDeterminadoAno(Integer ano);
+
+    Optional<List<Livro>> findByLinguagens(Linguagem linguagens);
+
 }
